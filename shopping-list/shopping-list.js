cb653f89a14b23a96d0ceeacdf775d47a0b075f6
SusanBird <susanbird391@gmail.com>
2022-03-29 11:14:09 -0700

    create and test form

--- shopping-list/shopping-list.js
+++ shopping-list/shopping-list.js
@@ -2,8 +2,19 @@ import { checkAuth, logout } from '../fetch-utils.js';
 
 checkAuth();
 
+const form = document.querySelector('form');
 const logoutButton = document.getElementById('logout');
 
 logoutButton.addEventListener('click', () => {
     logout();
+});
+
+form.addEventListener('submit', async (e) =>{
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    alert('it works');
+
+
 });
